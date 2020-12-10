d25a5f341969a37cf6166bb2e9826fef365a70a0
.Blue <yuueu@pm.me>
2020-12-10 20:46:10 +0900

    copy css function complete

--- src/logic.js
+++ src/logic.js
@@ -5,11 +5,15 @@ const bottomRight = document.getElementById('bottom-right');
 
 const preview = document.getElementById('app').children[1];
 
+const copyButton = document.getElementById('copy');
+
 function evaluate(val) {
+    copyButton.innerText = "Copy CSS";
+    copyButton.style.border = "1px solid #6c6c6c";
     let number = val.match(/\d+/g);
     let unit = val.match(/[pxem%]+/g);
 
-    if(unit === '' || unit === undefined || unit === null) {
+    if (unit === '' || unit === undefined || unit === null) {
         unit = "px";
     }
 
@@ -35,3 +39,49 @@ bottomLeft.addEventListener("input", (e) => {
     const [number, unit] = evaluate(e.target.value);
     preview.style.borderBottomLeftRadius = `${number}${unit}`;
 });
+
+copyButton.addEventListener('click', (e) => {
+    e.target.innerText = "Copied!";
+    e.target.style.border = "1px solid #37612b";
+
+    let tl = topLeft.value;
+    let tr = topRight.value;
+    let bl = bottomLeft.value;
+    let br = bottomRight.value;
+
+    if (tl.match(/[pxem%]+/g) === null) tl += "px";
+    if (tr.match(/[pxem%]+/g) === null) tr += "px";
+    if (bl.match(/[pxem%]+/g) === null) bl += "px";
+    if (br.match(/[pxem%]+/g) === null) br += "px";
+
+    const firefox = `-moz-border-bottom-left-radius: ${tl};
+    -moz-border-bottom-left-radius: ${tr};
+    -moz-border-bottom-left-radius: ${bl};
+    -moz-border-bottom-left-radius: ${br};`;
+
+    const safari = `-webkit-border-bottom-left-radius: ${tl};
+    -webkit-border-bottom-left-radius: ${tr};
+    -webkit-border-bottom-left-radius: ${bl};
+    -webkit-border-bottom-left-radius: ${br};`;
+
+    const css = `border-top-left-radius: ${tl};
+    border-top-right-radius: ${tr};
+    border-bottom-left-radius: ${bl};
+    border-bottom-right-radius: ${br};`;
+
+    let dummy = document.getElementById('dummy');
+    dummy.innerText = `{
+    ${css}
+    ${firefox}
+    ${safari}
+}`;
+
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(dummy);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    document.execCommand('copy');
+    selection.removeAllRanges();
+})
