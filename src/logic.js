30b6341f2dc2552980db915b87384c5b1fc4612d
.Blue <yuueu@pm.me>
2020-12-10 19:41:32 +0900

    basic functionality done

--- src/logic.js
+++ src/logic.js
@@ -1,1 +1,22 @@
-// javascript file
+const topLeft = document.getElementById('top-left');
+const topRight = document.getElementById('top-right');
+const bottomLeft = document.getElementById('bottom-left');
+const bottomRight = document.getElementById('bottom-right');
+
+const preview = document.getElementById('app').children[1];
+
+topLeft.addEventListener("input", (e) => {
+    preview.style.borderTopLeftRadius = e.target.value;
+});
+
+topRight.addEventListener("input", (e) => {
+    preview.style.borderTopRightRadius = e.target.value;
+});
+
+bottomRight.addEventListener("input", (e) => {
+    preview.style.borderBottomRightRadius = e.target.value;
+});
+
+bottomLeft.addEventListener("input", (e) => {
+    preview.style.borderBottomLeftRadius = e.target.value;
+});
